3cd52c10940c6930eb90189cba2f5ea7baf4d5e6
spdolan <snpdolan@gmail.com>
2020-08-30 11:42:12 -0400

    added travis settings.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 const withPlugins = require("next-compose-plugins");
 const optimizedImages = require("next-optimized-images");
 
-module.exports = withPlugins([optimizedImages]);
+module.exports = withPlugins([optimizedImages], {
+  basePath: "/blog",
+  assetPrefix: "/blog/", // assetPrefix requires the trailing slash
+});
